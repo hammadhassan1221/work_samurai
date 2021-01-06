d4cd258c885e5970e3bc09cf33317ea10aad3657
HammadHassan1 <hammadhassan@jinnbyte.com>
2021-01-06 23:11:48 +0500

    Firebase integrated in iOS

--- lib/screens/splash/splash_providers.dart
+++ lib/screens/splash/splash_providers.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:get_version/get_version.dart';
@@ -10,13 +11,21 @@ import 'package:work_samurai/screens/login/login.dart';
 import 'package:work_samurai/screens/worker/worker.dart';
 
 class SplashProvider extends ChangeNotifier {
-  String versionNumber="";
+  String versionNumber = "";
 
   BuildContext context;
+  FirebaseMessaging _fcm = FirebaseMessaging();
 
-  init({@required BuildContext context}) async{
+  init({@required BuildContext context}) async {
     await getVersionNumber();
+    getToken();
+
     this.context = context;
+  }
+
+  void getToken() async {
+    final token = await _fcm.getToken();
+    debugPrint('$token');
   }
 
   initializeResources(BuildContext context) {
